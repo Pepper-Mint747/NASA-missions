8ad89409062d01498a70aa759616f973564bce5f
Brian <omogabrian2060@outlook.com>
2021-07-10 15:03:13 +0300

    added Procfile

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
+require("dotenv").config()
 
-const MONGO_URL = "mongodb+srv://nasa-api:K1SGgdRO470c9GPL@training.nkgrw.mongodb.net/nasa?retryWrites=true&w=majority"
 
 mongoose.connection.once("open", () => {
     console.log("MongoDB connection ready!");
@@ -11,7 +11,7 @@ mongoose.connection.on("error", (err) => {
 });
 
 async function mongoConnect() {
-    await  mongoose.connect(MONGO_URL, {
+    await  mongoose.connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useFindAndModify: false,
         useCreateIndex: true,
